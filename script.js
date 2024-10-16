69590fc0dc07a80cd03eec91ac5aa5fe64cc7d2e
Esha-Zehra-72 <eshazehra72@gmail.com>
2024-10-16 06:02:12 -0500

    Forms

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+// Tooltips
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+// nav3 Buttons
+
+var button = document.getElementsByTagName("button")
+function clickedBtn(button){
+button.style.backgroundColor = "red"
+}
